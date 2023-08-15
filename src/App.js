06b67e023db9d5771cefe091496b88f39b7d1304
Khizer Ali <khizer358@gmail.com>
2023-08-15 23:33:52 +0500

    fetch api of random users

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
+function  App() {
+  const [user, setuser] = useState([]);
+
+  useEffect(() => {
+    async function getUsers(){
+      const response = await fetch("https://randomuser.me/api?results=5")
+      const data = await response.json()
+      setuser(data.results)
+    }
+  getUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random Users</h1>
+      <ul className='userContent'>
+        {user.map((user, index) => (
+          <>
+          <div className='container'>
+            <img className='userImage' src={user.picture.thumbnail}/>
+            
+          <div className='userData'>
+          <li className='userList' key={index}>
+            <p className='userName'>{user.name.first} {user.name.last}</p>
+            <p>{user.email}</p>
+          </li>
+          </div>
+          </div>
+          </>
+        ))}
+      </ul>
     </div>
+
   );
 }
 
