9a555323110129d4e40bb33d2f58b2836124f886
Khizer Ali <khizer358@gmail.com>
2023-08-16 13:04:03 +0500

    completed with refresh btn

--- src/App.js
+++ src/App.js
@@ -3,30 +3,51 @@ import './App.css';
 
 function  App() {
   const [user, setuser] = useState([]);
+  const [isSpinning, setIsSpinning] = useState(false);
 
-  useEffect(() => {
-    async function getUsers(){
-      const response = await fetch("https://randomuser.me/api?results=5")
-      const data = await response.json()
-      setuser(data.results)
+
+  async function getUsers() {
+    setIsSpinning(true);
+    const response = await fetch("https://randomuser.me/api?results=5");
+    const data = await response.json();
+    setuser(data.results);
+    setIsSpinning(false);
+  }
+  
+    useEffect(() => {
+      getUsers();
+    }, []);
+  
+    const handleRefresh = () => {
+      getUsers();
     }
-  getUsers();
-  }, []);
+  
 
   return (
     <div className="App">
       <h1>Random Users</h1>
+      <div className='reloadBtn'>
+      <svg onClick={handleRefresh} className={`w-6 h-6 ${isSpinning ? 'spinning' : ''}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+</svg>
+
+      </div>
       <ul className='userContent'>
         {user.map((user, index) => (
           <>
           <div className='container'>
-            <img className='userImage' src={user.picture.thumbnail}/>
+            <img className='userImage' src={user.picture.thumbnail} alt='User_image'/>
             
           <div className='userData'>
           <li className='userList' key={index}>
             <p className='userName'>{user.name.first} {user.name.last}</p>
             <p>{user.email}</p>
           </li>
+          </div>
+          <div className='messageBtn'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+</svg>
           </div>
           </div>
           </>
